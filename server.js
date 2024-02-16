6d070a9be981569ae561968d67cac44c2462e6dc
vasibhat <vasishtadbhat@gmail.com>
2024-02-16 15:46:38 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+ const express= require('express')
+ const app=express()
+ const mongoose = require('mongoose')
+ const ShortUrl = require('./models/shortUrl')
+
+ mongoose.connect('mongodb://localhost/url-shortener', {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
+
+
+ app.set('view engine','ejs')
+ app.use(express.urlencoded({ extended: false }))
+
+
+ app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render('index', { shortUrls: shortUrls })
+  })
+  
+
+
+app.post('/shortUrls', async (req,res)=>{
+   await  ShortUrl.create({full:req.body.fullUrl})
+   res.redirect('/')
+})
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+    if (shortUrl == null) return res.sendStatus(404)
+  
+    shortUrl.clicks++
+    shortUrl.save()
+  
+    res.redirect(shortUrl.full)
+  })
+
+ app.listen(process.env.port || 5000);
